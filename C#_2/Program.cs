f7de2563f62cd171472690d783db877b7949a8d2
user <user@WIN-UKQRC56FDU3>
2024-06-05 13:11:44 +0300

    Добавьте файлы проекта.

---
+++ C#_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количествло ступеней: ");
+            int length=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вы ввели число: ", length);
+            string s = " ";
+            
+            for (int i = 0; i < length; i++)
+            {
+                s += "  ";
+                Console.WriteLine("***");
+                Console.Write(s);
+                for (int j = 0; j < 1; j++) 
+                {
+                    Console.WriteLine("*");
+                }
+                Console.Write(s);
+            }
+        }
+    }
+}
